41aaab0a2f20b307ee7069a9a8c5ebf855ffe321
koromsergei <sv.korom@gmail.com>
2021-08-22 15:21:53 +0300

    Update Task_1.10.py

--- Python/Task_1.10.py
+++ Python/Task_1.10.py
@@ -1,9 +1,19 @@
+
+
 n = int(input())
 x = list()
 for i in range(n):
     a = tuple(input())
     x.append(a)
+<<<<<<< HEAD
 #for i in range(n):
     #for j in range(x[i]):
+=======
+for i in range(n):
+    for j in range(x[i]):
+
+#чета изменил)
+#и еще добавил
+>>>>>>> parent of 8187746 (хз че это)
 
 #if x[i][j]==a or x[i][j]==n or x[i][j]==t or x[i][j]==o or x[i][j]==n:
