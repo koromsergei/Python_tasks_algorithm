631a686539d6630c74df3a63d13c8cf0596eaca2
koromsergei <sv.korom@gmail.com>
2021-08-22 14:34:04 +0300

    хз че 2

--- Python/Task_1.10.py
+++ Python/Task_1.10.py
@@ -1,9 +1,13 @@
+
+
 n = int(input())
 x = list()
 for i in range(n):
     a = tuple(input())
     x.append(a)
+
 #for i in range(n):
     #for j in range(x[i]):
+
 
 #if x[i][j]==a or x[i][j]==n or x[i][j]==t or x[i][j]==o or x[i][j]==n:
