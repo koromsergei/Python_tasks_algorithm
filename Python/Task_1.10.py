7263dbbe68a27e7f387a06f730272eade45f6f64
koromsergei <sv.korom@gmail.com>
2021-08-22 15:35:55 +0300

    у

--- Python/Task_1.10.py
+++ Python/Task_1.10.py
@@ -5,21 +5,15 @@ x = list()
 for i in range(n):
     a = tuple(input())
     x.append(a)
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 #for i in range(n):
     #for j in range(x[i]):
-=======
-=======
->>>>>>> parent of 8187746 (хз че это)
+
 for i in range(n):
     for j in range(x[i]):
 
 #чета изменил)
 #и еще добавил
-<<<<<<< HEAD
->>>>>>> parent of 8187746 (хз че это)
-=======
->>>>>>> parent of 8187746 (хз че это)
+
 
 #if x[i][j]==a or x[i][j]==n or x[i][j]==t or x[i][j]==o or x[i][j]==n:
