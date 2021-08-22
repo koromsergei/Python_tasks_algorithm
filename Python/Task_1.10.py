5ac4f0d9e56c013cef363759dbcfa38ca2265c8a
koromsergei <sv.korom@gmail.com>
2021-08-22 15:41:42 +0300

    нвый

--- Python/Task_1.10.py
+++ Python/Task_1.10.py
@@ -9,11 +9,11 @@ for i in range(n):
 #for i in range(n):
     #for j in range(x[i]):
 
+
 for i in range(n):
     for j in range(x[i]):
 
 #чета изменил)
 #и еще добавил
-
 
 #if x[i][j]==a or x[i][j]==n or x[i][j]==t or x[i][j]==o or x[i][j]==n:
