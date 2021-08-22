f02c527f5b6bcdf88c1f33aff9fc1518838bfe36
koromsergei <sv.korom@gmail.com>
2021-08-22 15:03:15 +0300

    Revert "хз че 2"
    This reverts commit 631a686539d6630c74df3a63d13c8cf0596eaca2.

--- Python/Task_1.10.py
+++ Python/Task_1.10.py
@@ -3,9 +3,7 @@ x = list()
 for i in range(n):
     a = tuple(input())
     x.append(a)
-
 #for i in range(n):
     #for j in range(x[i]):
-
 
 #if x[i][j]==a or x[i][j]==n or x[i][j]==t or x[i][j]==o or x[i][j]==n:
