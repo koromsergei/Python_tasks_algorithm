933b94c7a6c97c718efec346d7ca5bee7c7be4f7
koromsergei <sv.korom@gmail.com>
2021-08-22 15:02:25 +0300

    ы

--- Python/Task_1.10.py
+++ Python/Task_1.10.py
@@ -10,4 +10,11 @@ for i in range(n):
     #for j in range(x[i]):
 
 
+<<<<<<< HEAD
+=======
+#чета изменил)
+#и еще добавил
+
+
+>>>>>>> parent of 8465f40 (хз че 2)
 #if x[i][j]==a or x[i][j]==n or x[i][j]==t or x[i][j]==o or x[i][j]==n:
