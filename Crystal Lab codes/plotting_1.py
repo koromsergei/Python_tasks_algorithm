c57cb1e94bb5d9847513544a733b29583c5f7cad
Sergei Koromyslov <sv.korom@gmail.com>
2021-08-23 15:30:41 +0300

    fit gauss try 1

--- Crystal Lab codes/plotting_1.py
+++ Crystal Lab codes/plotting_1.py
@@ -1,13 +1,24 @@
+import math
+
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import pylab as pl
-DATA = pd.read_excel("30.90.3.xlsx")
+import scipy.optimize as sc
 
-
+DATA = pd.read_excel("10.100.4.xlsx")
 x = DATA["Length"]
 x = x * 1000
-x.hist(bins=15)
-pl.suptitle("Au(30) / Si(90) структура 3")
+a = 1
+def gauss(a, x):
+    return ((1 / a*np.sqrt(2 * np.pi)) * np.exp((-x ** 2) / 2))
 
+y = np.linspace(0, len(x), 1)
+
+f = gauss(a, x)
+
+popt, pcov = sc.curve_fit(f, x, y)
+plt.grid(True)
+pl.suptitle("Au(30) / Si(90) структура 3")
 pl.show()
+plt.plot(x, popt)
