8153dcd8013e0b6aee7b1e658baf50c4458c9ea9
Sergej Koromyslov <sergej.koromyslov@metalab.ifmo.ru>
2022-10-21 18:01:54 +0300

    добавил алгоритм поиска в ширину3

--- grokaem/search_width_Chapter6.py
+++ grokaem/search_width_Chapter6.py
@@ -1,6 +1,10 @@
-
-
 graph = {}
-
-graph [ "you"] = [ "alice", " ЬоЬ", "claire"]
+fruit = "mango"
+graph["you"] = ["alice", " ЬоЬ", "claire"]
 print(graph)
+for i in range(len(graph) - 1):
+
+    if fruit in graph:
+        print("Found!")
+    else:
+        print(graph.get())
