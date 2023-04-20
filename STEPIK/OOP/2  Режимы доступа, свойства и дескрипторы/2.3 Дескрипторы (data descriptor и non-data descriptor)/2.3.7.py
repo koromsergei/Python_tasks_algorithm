2355f734142dcee854470cb316062492dec8cd3e
Sergej Koromyslov <sergej.koromyslov@metalab.ifmo.ru>
2023-04-20 17:09:26 +0300

    238

--- STEPIK/OOP/2  Режимы доступа, свойства и дескрипторы/2.3 Дескрипторы (data descriptor и non-data descriptor)/2.3.7.py
+++ STEPIK/OOP/2  Режимы доступа, свойства и дескрипторы/2.3 Дескрипторы (data descriptor и non-data descriptor)/2.3.7.py
@@ -1,4 +1,18 @@
+class ValidateString:
+    def __init__(self, min_length, max_length):
+        self.min_length = min_length
+        self.max_length = max_length
+
+    def validate(self, string):
+
+        if not (type(string) is str) or not (self.min_length <= len(string) <= self.max_length):
+            return False
+        return True
+
+
 class StringValue:
+    def __init__(self, validator: ValidateString):
+        self.validator = validator
 
     def __set_name__(self, owner, name):
         self.name = '_' + name
@@ -7,24 +21,16 @@ class StringValue:
         return getattr(instance, self.name)
 
     def __set__(self, instance, value):
-        setattr(instance, self.name, value)
-
-
-class ValidateString:
-    def __init__(self, min_length, max_length):
-        self.min_length = min_length
-        self.max_length = max_length
-
-    def validate(self, string):
-        if not (type(string) is str) or self.min_length <= len(string) <= self.max_length:
-            return False
-        return True
+        if self.validator.validate(value):
+            setattr(instance, self.name, value)
+        else:
+            pass
 
 
 class RegisterForm:
-    login = StringValue()
-    password = StringValue()
-    email = StringValue()
+    login = StringValue(validator=(ValidateString(1, 10)))
+    password = StringValue(validator=(ValidateString(1, 10)))
+    email = StringValue(validator=(ValidateString(1, 2)))
 
     def __init__(self, login, password, email):
         self.login = login
@@ -42,6 +48,6 @@ class RegisterForm:
               '</form>', sep='\n')
 
 
-
-from = RegisterForm('Login', 'Password', 'email')
-
+form = RegisterForm('Sergei', 'df123l30L', 'w@gmail.com')
+form.show()
+form.get_field()
