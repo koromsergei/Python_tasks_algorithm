93a4a406d76e579522c624c44c000b74776d015f
Sergej Koromyslov <sergej.koromyslov@metalab.ifmo.ru>
2023-04-25 17:52:04 +0300

    3.2.4

---
+++ STEPIK/OOP/3  Магические методы классов/3.2 Метод __call__. Функторы и классы-декораторы/3.2.4.py
@@ -0,0 +1,41 @@
+from string import ascii_lowercase, digits
+
+
+class LoginForm:
+    def __init__(self, name, validators=None):
+        self.name = name
+        self.validators = validators
+        self.login = ""
+        self.password = ""
+
+    def post(self, request):
+        self.login = request.get('login', "")
+        self.password = request.get('password', "")
+
+    def is_validate(self):
+        if not self.validators:
+            return True
+
+        for v in self.validators:
+            if not v(self.login) or not v(self.password):
+                return False
+
+        return True
+
+
+# здесь прописывайте классы валидаторов: LengthValidator и CharsValidator
+
+class LengthValidator:
+    def __init__(self, min_length, max_length):
+        self.min_length = min_length
+        self.max_length = max_length
+
+    def __call__(self, *args, **kwargs):
+
+
+
+class CharsValidator:
+    def __init__(self, chars):
+        self.chars = chars
+
+
