0433c3d1b15c9630405be4e79416d4ec61f212c6
Sergei Koromyslov <sv.korom@gmail.com>
2023-05-19 18:34:41 +0300

    4.3.7 в процессе

--- STEPIK/OOP/4  Наследование и полиморфизм/4.3 Наследование. Функция super() и делегирование/4.3.7.py
+++ STEPIK/OOP/4  Наследование и полиморфизм/4.3 Наследование. Функция super() и делегирование/4.3.7.py
@@ -1,23 +1,26 @@
-def integer_params(cls):
-    def integer_params_decorated(*args):
-        print(args)
+def integer_params(func):
+    def inner(*args):
         for i in args:
-            print(i)
+            print(i, type(i) is not int)
             if type(i) is not int:
                 raise TypeError
-        methods = {k: v for k, v in cls.__dict__.items() if callable(v)}
-        for k, v in methods.items():
-            print(k, v)
-            setattr(cls, k, integer_params_decorated(v))
+        return func(args)
 
-        return cls
-    return integer_params_decorated
+    return inner
+
+
+def integer_params_decorated(cls):
+    methods = {k: v for k, v in cls.__dict__.items() if callable(v)}
+    for k, v in methods.items():
+        setattr(cls, k, integer_params_decorated(v))
+
+    return cls
 
 
 @integer_params
 class Vector:
     def __init__(self, *args):
-        self.__coords = list(args)
+        self.__coords = list(*args)
 
     def __getitem__(self, item):
         return self.__coords[item]
@@ -31,5 +34,7 @@ class Vector:
 
 
 vector = Vector(1, 2)
+
 print(vector[1])
-vector[1] = 20.4  # TypeError
+vector[1] = 20.4
+print(vector[1])
