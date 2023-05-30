b25c827b46bb29395f9cdfd7435f8c6735230b07
Sergej Koromyslov <sergej.koromyslov@metalab.ifmo.ru>
2023-05-30 15:59:40 +0300

    5.5.5 done

--- STEPIK/OOP/5  Исключения и менеджеры контекста/5.5 Менеджеры контекстов. Оператор with/5.5.5.py
+++ STEPIK/OOP/5  Исключения и менеджеры контекста/5.5 Менеджеры контекстов. Оператор with/5.5.5.py
@@ -4,6 +4,14 @@ class Box:
         self._max_weight = max_weight
         self._thing = list()
 
+    @property
+    def thing(self):
+        return self._thing
+
+    @thing.setter
+    def thing(self, value):
+        self._thing = value
+
     def add_thing(self, obj: tuple):
         self._thing.append(obj)
         if sum([i[1] for i in self._thing]) > self._max_weight:
@@ -13,17 +21,15 @@ class Box:
 class BoxDefender:
     def __init__(self, box: Box):
         self._box = box
+        self._thing_t = box.thing[:]
 
     def __enter__(self):
-        return self
+        return self._box
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        print(exc_type, exc_val, exc_tb)
         if exc_type is not None:
+            self._box.thing = self._thing_t
             raise Exception
-
-    def add_thing(self, obj: tuple):
-        self._box.add_thing(obj)
 
 
 box = Box("сундук", 1000)
@@ -32,8 +38,7 @@ box.add_thing(("рубашка", 134))
 box.add_thing(("стол", 134))
 
 with BoxDefender(box) as b:
-    b.add_thing(box.add_thing(("стул", 134)))
-    b.add_thing(box.add_thing(("стульчик", 1034)))
+    b.add_thing((("стул", 134)))
+    b.add_thing((("стульчик", 100)))
 
-print(box._thing)
-
+print(box._thing, 'box')
